6c51f56fc3d257b108008b5eccfaea4375efec8a
Kang Choi / 최 강 <109474668+y2hscmtk@users.noreply.github.com>
2023-12-31 01:10:31 +0900

    Section 2-3 : API 방식

--- spring-introduction/src/main/java/com/example/springintroduction/controller/HelloController.java
+++ spring-introduction/src/main/java/com/example/springintroduction/controller/HelloController.java
@@ -1,9 +1,11 @@
 package com.example.springintroduction.controller;
 
+import com.example.springintroduction.model.Hello;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 // MVC Pattern (Model - View - Model)
 // @Controller annotation을 사용하면 해당 클래스를 Controller로서 사용하도록 해줌
@@ -11,6 +13,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class HelloController {
 
+
+    // << MVC 템플릿 엔진 >>
     // api에서 /hello로 들어올 경우 해당 함수를 호출함
     // 사용자가 /hello로 접속할 경우 View는 hello.html을 연결하고,
     // Model클래스를 사용하여 "data"라는 키 값으로 "hello!"라는 데이터를 전달하는 역할을 수행함(Controller)
@@ -37,4 +41,31 @@ public class HelloController {
         // 뷰 리졸버를 통해 hello-template.html을 찾아 화면을 렌더링한다.
         return "hello-template";
     }
+
+    // << API 방식 예시 >>
+    // @ResponseBody : html통신 프로토콜의 body부에 응답 내용을 직접 넣어주겠다는 의미
+    // ResponseBody annotation이 있으면 뷰 리졸버로 연결하지 않는다.
+    @GetMapping("hello-string")
+    @ResponseBody
+    public String helloString(@RequestParam("name") String name){
+        // ex) name = spring => "hello spring"
+        // ResponseBody annotation 으로 선언함으로서 요청한 클라이언트에 데이터("hello spring")가 그대로 전달됨
+        return "hello " + name;
+    }
+
+    // Command + Shift + Enter => ; 자동완성
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name) {
+        Hello hello = new Hello();
+
+        hello.setName(name);
+
+        return hello;
+    }
+
+
+
+
+
 }
---
+++ spring-introduction/src/main/java/com/example/springintroduction/model/Hello.java
@@ -0,0 +1,14 @@
+package com.example.springintroduction.model;
+
+// api 요청 테스트용 class
+public class Hello {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}

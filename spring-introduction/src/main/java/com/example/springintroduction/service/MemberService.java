e3b3457faa48ef781d05afac811cf8e5e7a9c7aa
Kang Choi / 최 강 <109474668+y2hscmtk@users.noreply.github.com>
2024-01-01 21:11:09 +0900

    Section 3-4 : 회원 서비스 개발

---
+++ spring-introduction/src/main/java/com/example/springintroduction/service/MemberService.java
@@ -0,0 +1,53 @@
+package com.example.springintroduction.service;
+
+import com.example.springintroduction.domain.Member;
+import com.example.springintroduction.repository.MemberRepository;
+import com.example.springintroduction.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    /**
+     * 회원 가입
+     */
+    public Long join(Member member) {
+        // result => Optional로 반환됨
+//        Optional<Member> result = memberRepository.findByName(member.getName());
+//        // 반환된 Optional의 값이 null이 아니라면
+//        result.ifPresent(m -> {
+//            throw new IllegalStateException("이미 존재하는 회원입니다.");
+//        });
+
+        // ctrl + t => 일부 코드를 통째로 함수화 할 수 있음
+        validateDuplicatedMember(member);
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    // 중복회원 검증 로직
+    private void validateDuplicatedMember(Member member) {
+        memberRepository.findByName(member.getName())
+                .ifPresent(m -> {
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+                });
+    }
+
+    /**
+     * 전체 회원 조회
+     */
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    /**
+     * 특정 회원 조회
+     */
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+
+}

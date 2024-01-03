40d459fc92483d23a6059bd93cab3d85ad50ecf3
Kang Choi / 최 강 <109474668+y2hscmtk@users.noreply.github.com>
2024-01-03 13:25:00 +0900

    Section 6-2 : 순수 JDBC

--- spring-introduction/src/main/java/com/example/springintroduction/SpringConfig.java
+++ spring-introduction/src/main/java/com/example/springintroduction/SpringConfig.java
@@ -1,14 +1,25 @@
 package com.example.springintroduction;
 
+import com.example.springintroduction.repository.JdbcMemberRepository;
 import com.example.springintroduction.repository.MemberRepository;
 import com.example.springintroduction.repository.MemoryMemberRepository;
 import com.example.springintroduction.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
 
 // 자바 코드로 스프링 빈 등록하는 방법
 @Configuration
 public class SpringConfig {
+
+    private DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
 
     @Bean
@@ -19,7 +30,8 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+        //return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 
 }

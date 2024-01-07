a87c978f9f4b2ed50c2445a6a40a49dea6729f38
Kang Choi / 최 강 <109474668+y2hscmtk@users.noreply.github.com>
2024-01-07 18:01:38 +0900

    project init

---
+++ spring-core-basic/src/test/java/spring/springcorebasic/SpringCoreBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.springcorebasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCoreBasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

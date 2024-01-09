f2a27f0538771e60b979fe21b9640dbe786ba630
Kang Choi / 최 강 <109474668+y2hscmtk@users.noreply.github.com>
2024-01-09 21:12:24 +0900

    Section 3-4,3-5 : AppConfig 리펙토링, 새로운 구조와 할인 정책 적용

--- spring-core-basic/src/main/java/spring/springcorebasic/AppConfig.java
+++ spring-core-basic/src/main/java/spring/springcorebasic/AppConfig.java
@@ -1,6 +1,8 @@
 package spring.springcorebasic;
 
+import spring.springcorebasic.discount.DiscountPolicy;
 import spring.springcorebasic.discount.FixDiscountPolicy;
+import spring.springcorebasic.discount.RateDiscountPolicy;
 import spring.springcorebasic.member.MemberService;
 import spring.springcorebasic.member.MemberServiceImpl;
 import spring.springcorebasic.member.MemoryMemberRepository;
@@ -13,13 +15,23 @@ public class AppConfig {
 
     // 멤버 서비스 역할 배정
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        // Command + Option + m => 부분 함수화
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    private static MemoryMemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     // 주문 서비스 역할 배정
     // DIP를 만족하도록 OrderServiceImpl을 생성자 주입으로 의존관계 주입 설정
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(),new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(),discountPolicy());
+    }
+
+    public DiscountPolicy discountPolicy() {
+        //return new FixDiscountPolicy();
+        return new RateDiscountPolicy(); // 할인 정책 변경시, 이정도의 코드 수정으로 충분하다.
     }
 
 

a17d16f5c97993873413960ef43b8a7016bf71cb
Kang Choi / 최 강 <109474668+y2hscmtk@users.noreply.github.com>
2024-01-11 15:30:04 +0900

    Section 5-4 : 싱글톤 방식의 문제점

---
+++ spring-core-basic/src/test/java/spring/springcorebasic/singletom/StatefulService.java
@@ -0,0 +1,22 @@
+package spring.springcorebasic.singletom;
+
+public class StatefulService {
+
+    // 싱글톤 객체이므로 price는 여러쓰레드들 사이에서 공유되는 필드이다.
+    // private int price; //상태를 유지하는 필드 (stateful)
+    // 공유필드를 사용하지 않아야한다.
+//
+//    public void order(String name, int price) {
+//        System.out.println("name = " + name + " price = " + price);
+//        this.price = price; // 이런식으로 작성하면 문제가 발생한다!
+//    }
+
+    public int order(String name, int price) {
+        System.out.println("name = " + name + " price = " + price);
+        return price; // Stateless한 설계
+    }
+
+//    public int getPrice() {
+//        return price;
+//    }
+}
---
+++ spring-core-basic/src/test/java/spring/springcorebasic/singletom/StatefulServiceTest.java
@@ -0,0 +1,42 @@
+package spring.springcorebasic.singletom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import javax.swing.plaf.PanelUI;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StatefulServiceTest {
+
+
+    @Test
+    void statefulServiceSingleton() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(TestConfig.class);
+        StatefulService statefulService1 = ac.getBean(StatefulService.class);
+        StatefulService statefulService2 = ac.getBean(StatefulService.class);
+
+        //다중 쓰레드 환경이라고 가정
+        //Thread1 : A사용자 10000원 주문
+        int userAPrice = statefulService1.order("userA",10000);
+        //Thread2 : B사용자 20000원 주문
+        int userBPrice = statefulService2.order("userB",20000);
+
+        //ThreadA : 사용자A 주문 금액 조회
+        //int price = statefulService1.getPrice();
+
+        //같은 객체 인스턴스를 사용하기 때문에, price의 값이 유지되어 사용자A의 price가 사용자B의 price로 덮어씌워진다.
+        System.out.println("price = " + userAPrice);
+    }
+
+    static class TestConfig {
+
+        @Bean
+        public StatefulService statefulService() {
+            return new StatefulService();
+        }
+    }
+
+
+}

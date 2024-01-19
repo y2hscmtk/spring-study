bbce7b9c9cc34a2293a0c96a0832928f590f102f
Kang Choi / 최 강 <109474668+y2hscmtk@users.noreply.github.com>
2024-01-19 15:18:34 +0900

    Section 8-2 : 인터페이스 InitaillizingBean, DisposableBean

--- spring-core-basic/src/test/java/lifecycle/NetworkClient.java
+++ spring-core-basic/src/test/java/lifecycle/NetworkClient.java
@@ -1,11 +1,15 @@
 package lifecycle;
 
-public class NetworkClient {
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+public class NetworkClient implements InitializingBean, DisposableBean {
+
     private String url;
     public NetworkClient() {
         System.out.println("생성자 호출, url = " + url);
-        connect();
-        call("초기화 연결 메시지");
+//        connect();
+//        call("초기화 연결 메시지");
     }
 
     public void setUrl(String url) {
@@ -18,11 +22,25 @@ public class NetworkClient {
     }
 
     public void call(String message) {
-        System.out.println("call : " + url + "message = " + message);
+        System.out.println("call : " + url + " message = " + message);
     }
 
     // 서비스 종료시 호출
     public void disconnect() {
         System.out.println("close = " + url);
+    }
+
+    // InitializaingBean implementation
+    // 의존 관계 주입이 끝나고 호출
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        connect();
+        call("초기화 연결 메시지");
+    }
+
+    // 빈이 종료될 때 호출됨
+    @Override
+    public void destroy() throws Exception {
+        disconnect();
     }
 }

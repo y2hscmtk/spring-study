82e6deeb2241dbe05ce3f6fc236bb0107ff91500
Kang Choi / 최 강 <109474668+y2hscmtk@users.noreply.github.com>
2024-01-20 22:46:13 +0900

    Section 9-5 : request 스코프 예제 만들기

---
+++ spring-core-basic/src/main/java/spring/springcorebasic/common/MyLogger.java
@@ -0,0 +1,41 @@
+package spring.springcorebasic.common;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+import java.util.UUID;
+
+
+// 웹 스코프는 요청 하나당 하나 생성되고, 요청이 끝나면 소멸된다.
+@Component
+@Scope(value = "request")
+public class MyLogger {
+
+    private String uuid;
+    private String requestURL;
+
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
+    }
+
+    public void setRequestURL(String requestURL) {
+        this.requestURL = requestURL;
+    }
+
+    public void log(String meesage) {
+        System.out.println("[ " + uuid + "]" + "[" + requestURL + "]" + meesage);
+    }
+
+    @PostConstruct
+    public void init() {
+        uuid = UUID.randomUUID().toString(); // 고유한 값으로 생성
+        System.out.println("[" + uuid + "] request scope bean create:" + this);
+    }
+
+    @PreDestroy
+    public void close() {
+        System.out.println("[" + uuid + "] request scope bean close:" + this);
+    }
+}
---
+++ spring-core-basic/src/main/java/spring/springcorebasic/web/LogDemoController.java
@@ -0,0 +1,31 @@
+package spring.springcorebasic.web;
+
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import spring.springcorebasic.common.MyLogger;
+
+@Controller
+@RequiredArgsConstructor // 멤버변수에 대한 의존관계 자동 주입
+public class LogDemoController {
+
+    private final LogDemoService logDemoService;
+    private final MyLogger myLogger;
+    // 현재 request 스콥으로 웹스코프 빈으로 설정되어있음 => 요청이 들어와야 생성이됨 => RequireArgsConstructor 오류발생
+    // => 없는 빈을 내놓으라고 하기 때문이다.
+    // => Provider를 사용하여 해결 가능
+
+    @RequestMapping("log-demo")
+    @ResponseBody
+    public String logDemo(HttpServletRequest request) {
+        String requestURL = request.getRequestURL().toString();
+        myLogger.setRequestURL(requestURL);
+
+        myLogger.log("contorller test");
+        logDemoService.logic("testId");
+
+        return "OK";
+    }
+}

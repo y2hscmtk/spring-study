b7469e24c104fbf2ef75db0fb1e200d59605dc48
Kang Choi / 최 강 <109474668+y2hscmtk@users.noreply.github.com>
2024-01-24 21:14:29 +0900

    Section 3-1 : 영속성 컨텍스트 1

--- orm-jpa-basic/src/main/java/hellojpa/JpaMain.java
+++ orm-jpa-basic/src/main/java/hellojpa/JpaMain.java
@@ -20,6 +20,32 @@ public class JpaMain {
         // UPDATE MEMBER 회원 정보 수정
         updateMember(2L);
 
+
+        /**
+         * 엔티티 매니저와 영속성 컨텍스트
+         * - 엔티티 매니저는 엔티티 매니저 펙토리에 의해 요청이 들어올때마다 새롭게 생성된다.
+         * - 영속성 컨텍스트는 엔티티 매니저에 속한 것으로, 엔티티를 영구 저장하는 환경을 말한다.
+         * */
+
+        // 1. 비영속 상태 => 엔티티 매니저와 아무런 관련이 없는 상태, DB에 들어가지도 않는다.
+        Member member = new Member();
+        member.setName("HelloJPA");
+        member.setId(100L);
+
+        // 2. 영속 상태
+        // member는 persist 명령에 의해 영속상태가 됨, DB에 저장되지는 않음(BEFORE ~~ AFTER 안에 쿼리문 존재하는지 확인)
+        System.out.println("=== BEFORE ===");
+        em.persist(member);
+        System.out.println("=== AFTER ===");
+
+        // 3. 준영속, 삭제
+        // detach 명령을 통해 영속성 컨텍스트에서 회원 엔티티를 분리, 준영속 상태가 됨
+        // em.detach(member);
+        // 객체를 삭제한 상태(삭제)
+        // em.remove(member);
+
+
+
         // 작업이 끝나면 EntityManager, EntityMangagerFactory 종료
         em.close();
         emf.close();
@@ -55,6 +81,9 @@ public class JpaMain {
         tx.commit(); // 작업 완료 후 상태 반영(commit)
     }
 
+    /**
+     * UPDATE MEMBER 회원 정보 업데이트
+     * */
     private static void updateMember(Long id){
         EntityTransaction tx = em.getTransaction(); // 트렌젝션 생성
         tx.begin(); // 트랜젝션 시작

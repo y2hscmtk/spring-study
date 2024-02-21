b715d75a84f5c3634f186d14560b3cf2f682602b
Kang Choi / 최 강 <109474668+y2hscmtk@users.noreply.github.com>
2024-02-21 15:00:31 +0900

    Section 5-1 : 상품 엔티티 개발(비즈니스 로직 추가)

--- spring-utilizing-jpa1/jpashop/src/main/java/utilizingjpa/jpashop/domain/item/Item.java
+++ spring-utilizing-jpa1/jpashop/src/main/java/utilizingjpa/jpashop/domain/item/Item.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 import utilizingjpa.jpashop.domain.Category;
+import utilizingjpa.jpashop.exception.NotEnoughStockException;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -24,4 +25,25 @@ public abstract class Item {
     // 다대다 관계(실무 사용 금지)
     @ManyToMany(mappedBy = "items") // items에 의해 매핑될 것이다.
     private List<Category> categories = new ArrayList<>();
+
+    //==비즈니스 로직==//
+    //Setter를 사용하는 것이 아닌, 비즈니스 메소드를 별도로 작성하여 사용해야 한다.
+    /**
+     * stock 증가
+     * */
+    public void addStock(int stockQuantity) {
+        this.stockQuantity += stockQuantity;
+    }
+
+    /**
+     * stock 감소
+     * */
+    public void removeStock(int stockQuantity) {
+        int restStock = this.stockQuantity - stockQuantity;
+        // 0보다 작아지면 안됨
+        if (restStock < 0) {
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = restStock;
+    }
 }

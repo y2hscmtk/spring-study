6c8724f2b2f6dfcd799a4a662513895b4f1c40f4
Kang Choi / 최 강 <109474668+y2hscmtk@users.noreply.github.com>
2024-05-27 16:04:30 +0900

    Session 4-6 주문 조회 V5 : JPA에서 DTO 직접 조회 - 컬렉션 조회 최적화

--- spring-utilizing-jpa1/jpashop/src/main/java/utilizingjpa/jpashop/repository/order/query/OrderQueryRepository.java
+++ spring-utilizing-jpa1/jpashop/src/main/java/utilizingjpa/jpashop/repository/order/query/OrderQueryRepository.java
@@ -5,6 +5,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 // OrderRepository : Order 엔티티를 조회하기 위함 => 핵심 비즈니스 로직
 // OrderQueryRepository : 화면, API에 의존 관계가 있는 전용 쿼리 레파지토리
@@ -42,5 +44,31 @@ public class OrderQueryRepository {
                                 " join o.member m" +
                                 " join o.delivery d", OrderQueryDto.class)
                 .getResultList();
+    }
+
+    public List<OrderQueryDto> findAllByDto_optimization() {
+        List<OrderQueryDto> result = findOrders();
+
+        // id 얻어오기
+        List<Long> orderIds = result.stream()
+                .map(o -> o.getOrderId())
+                .collect(Collectors.toList());
+
+        // id를 바탕으로 in 쿼리 생성
+        List<OrderItemQueryDto> orderItems = em.createQuery(
+                        "select new utilizingjpa.jpashop.repository.order.query.OrderItemQueryDto(oi.order.id,i.name,oi.orderPrice,oi.count)" +
+                                " from OrderItem oi" +
+                                " join oi.item i" +
+                                " where oi.order.id in :orderIds", OrderItemQueryDto.class)
+                .setParameter("orderIds", orderIds)
+                .getResultList();
+
+        Map<Long, List<OrderItemQueryDto>> orderItemMap = orderItems.stream()
+                .collect(Collectors.groupingBy(orderItemQueryDto -> orderItemQueryDto.getOrderId()));
+
+        // 메모리에서 매칭하여 값 세팅
+        result.forEach(o -> o.setOrderItems(orderItemMap.get(o.getOrderId())));
+
+        return result;
     }
 }

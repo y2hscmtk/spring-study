0967bf8825df1ec3de3eee37710f132b6c3f3a12
Kang Choi / 최 강 <109474668+y2hscmtk@users.noreply.github.com>
2024-05-28 23:03:40 +0900

    🎉 실전! 스프링 데이터 JPA : 프로젝트 설정

---
+++ spring-data-jpa/src/main/java/study/spring_data_jpa/controller/HelloController.java
@@ -0,0 +1,13 @@
+package study.spring_data_jpa.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping
+    public String hello() {
+        return "hello";
+    }
+}

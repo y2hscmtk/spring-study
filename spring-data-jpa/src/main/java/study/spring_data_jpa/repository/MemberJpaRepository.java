1c168f8f780059d9a95effb51f70ccb662a5283e
Kang Choi / 최 강 <109474668+y2hscmtk@users.noreply.github.com>
2024-05-29 01:10:58 +0900

    ✅ Session 3-1 : 순수 JPA 기반 리포지토리 만들기

--- spring-data-jpa/src/main/java/study/spring_data_jpa/repository/MemberJpaRepository.java
+++ spring-data-jpa/src/main/java/study/spring_data_jpa/repository/MemberJpaRepository.java
@@ -5,6 +5,9 @@ import jakarta.persistence.PersistenceContext;
 import org.springframework.stereotype.Repository;
 import study.spring_data_jpa.entity.Member;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public class MemberJpaRepository {
 
@@ -18,5 +21,26 @@ public class MemberJpaRepository {
 
     public Member find(Long id) {
         return em.find(Member.class, id);
+    }
+
+    // 삭제
+    public void delete(Member member) {
+        em.remove(member);
+    }
+
+    // 전체 조회
+    public List<Member> findAll() {
+        return em.createQuery("select m from Member m",Member.class)
+                .getResultList();
+    }
+
+    public Optional<Member> findById(Long id) {
+        Member member = em.find(Member.class, id);
+        return Optional.ofNullable(member); // Null일수도 있으므로
+    }
+
+    public long count() {
+        return em.createQuery("select count(m) from Member m", Long.class)
+                .getSingleResult();
     }
 }

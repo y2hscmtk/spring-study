2e859ee356c271270ce17e51e2b11cf9793f1efc
Kang Choi / 최 강 <109474668+y2hscmtk@users.noreply.github.com>
2024-06-14 00:12:05 +0900

    ✨ 사용자 비밀번호 암호화 설정 추가, 권한 접두사 작성

--- Spring-Security-Study/src/main/java/com/example/springsecuritystudy/service/JoinService.java
+++ Spring-Security-Study/src/main/java/com/example/springsecuritystudy/service/JoinService.java
@@ -4,6 +4,7 @@ import com.example.springsecuritystudy.dto.JoinDTO;
 import com.example.springsecuritystudy.entity.UserEntity;
 import com.example.springsecuritystudy.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,14 +13,17 @@ public class JoinService {
 
     private final UserRepository userRepository;
 
+    // 회원가입시 사용자 비밀번호 암호화
+    private final BCryptPasswordEncoder passwordEncoder;
+
     // 회원가입 진행
     public void joinProcess(JoinDTO joinDTO) {
 
         // 저장할 사용자 생성
         UserEntity user = UserEntity.builder()
                 .username(joinDTO.getUsername())
-                .password(joinDTO.getPassword())
-                .role("USER")
+                .password(passwordEncoder.encode(joinDTO.getPassword())) // 비밀번호 암호화
+                .role("ROLE_USER") // 권한 부여시 "ROLE_" 접두사를 앞에 붙여주어야 한다.
                 .build();
 
         userRepository.save(user);

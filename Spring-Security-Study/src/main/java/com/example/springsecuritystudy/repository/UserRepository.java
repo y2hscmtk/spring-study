b1943b65cd0ba09040adf87cd49d22501764d8c3
Kang Choi / 최 강 <109474668+y2hscmtk@users.noreply.github.com>
2024-06-14 23:34:47 +0900

    ✨ 중복회원방지 함수 작성

--- Spring-Security-Study/src/main/java/com/example/springsecuritystudy/repository/UserRepository.java
+++ Spring-Security-Study/src/main/java/com/example/springsecuritystudy/repository/UserRepository.java
@@ -4,4 +4,7 @@ import com.example.springsecuritystudy.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
+
+    // 회원 이름 중복 방지용
+    boolean existsByUsername(String username); // 해당하는 회원이 존재하는지 확인
 }

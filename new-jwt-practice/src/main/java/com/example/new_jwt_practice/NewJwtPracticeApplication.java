7fe664e8a8298dce11ca4799f5b5a42e2f6f6655
Kang Choi / 최 강 <109474668+y2hscmtk@users.noreply.github.com>
2024-06-17 22:25:55 +0900

    🎉 스프링 시큐리티 JWT 프로젝트 생성

---
+++ new-jwt-practice/src/main/java/com/example/new_jwt_practice/NewJwtPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.new_jwt_practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewJwtPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewJwtPracticeApplication.class, args);
+	}
+
+}
---
+++ new-jwt-practice/src/test/java/com/example/new_jwt_practice/NewJwtPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.new_jwt_practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewJwtPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

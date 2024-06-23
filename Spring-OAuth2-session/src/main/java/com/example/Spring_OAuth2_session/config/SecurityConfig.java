99bf56ad23d69613d0729db2ac34898cb69a8977
Kang Choi / 최 강 <109474668+y2hscmtk@users.noreply.github.com>
2024-06-23 20:49:51 +0900

    ✨ OAuth2를 위한 SecurityConfig 설정

---
+++ Spring-OAuth2-session/src/main/java/com/example/Spring_OAuth2_session/config/SecurityConfig.java
@@ -0,0 +1,36 @@
+package com.example.Spring_OAuth2_session.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity // 스프링 시큐리티 활성화
+public class SecurityConfig {
+
+    // 시큐리티 필터 활성화
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        // csrf, 폼 로그인 방식, 베이직 로그인 방식 비활성화
+        http
+                .csrf(csrf -> csrf.disable())
+                .formLogin(login -> login.disable())
+                .httpBasic(basic -> basic.disable());
+
+        // OAuth2 설정
+        http
+                .oauth2Login(Customizer.withDefaults());
+
+        // "/", "/oauth2/**", "/login/**" 경로에 대해서는 모두 허용
+        // 그 외의 요청에 대해서는 인증된 사용자에 한해서 허용
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/", "/oauth2/**", "/login/**").permitAll()
+                        .anyRequest().authenticated());
+
+        return http.build();
+    }
+}

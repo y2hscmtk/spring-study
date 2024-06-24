5dfaa7021e0513e3faa07827316195c97c76611f
Kang Choi / 최 강 <109474668+y2hscmtk@users.noreply.github.com>
2024-06-25 00:22:52 +0900

    ✅ 엔티티, 레포지토리 작성

---
+++ Spring-OAuth2-session/src/main/java/com/example/Spring_OAuth2_session/entity/Member.java
@@ -0,0 +1,18 @@
+package com.example.Spring_OAuth2_session.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "MEMBER")
+public class Member { // OAuth2 로그인에 성공한 회원을 저장하기 위한 엔티티
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String email;
+    private String role;
+}

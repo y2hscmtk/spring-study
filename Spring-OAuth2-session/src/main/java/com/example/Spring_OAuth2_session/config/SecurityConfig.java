8070816b172501b76eba71bc66a584d892a35bab
Kang Choi / 최 강 <109474668+y2hscmtk@users.noreply.github.com>
2024-07-01 22:30:53 +0900

    ✨ OAuth2 커스텀 로그인 페이지 연결

--- Spring-OAuth2-session/src/main/java/com/example/Spring_OAuth2_session/config/SecurityConfig.java
+++ Spring-OAuth2-session/src/main/java/com/example/Spring_OAuth2_session/config/SecurityConfig.java
@@ -29,8 +29,9 @@ public class SecurityConfig {
         // OAuth2 설정
         // 커스텀 서비스 등록
         http
-                .oauth2Login(oauth2 ->
-                        oauth2.userInfoEndpoint(userInfoEndpointConfig ->
+                .oauth2Login(oauth2 -> oauth2
+                        .loginPage("/login") // oauth2 커스텀 로그인 페이지 등록
+                        .userInfoEndpoint(userInfoEndpointConfig ->
                                 userInfoEndpointConfig.userService(customOAuth2UserService)));
 
         // "/", "/oauth2/**", "/login/**" 경로에 대해서는 모두 허용

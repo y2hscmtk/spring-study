fdca0a04fdc808584aa8afd735ab1b43ae668d9f
Kang Choi / 최 강 <109474668+y2hscmtk@users.noreply.github.com>
2024-07-02 20:24:40 +0900

    🎉 Spring OAuth2 JWT Practice Project 생성

---
+++ oauth2-jwt-api/src/main/java/com/example/oauth2_jwt_api/Oauth2JwtApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.oauth2_jwt_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oauth2JwtApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Oauth2JwtApiApplication.class, args);
+	}
+
+}
---
+++ oauth2-jwt-api/src/test/java/com/example/oauth2_jwt_api/Oauth2JwtApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.oauth2_jwt_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oauth2JwtApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

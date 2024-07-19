ac8c075d5b53802b8cfe627aad3edc0201911c2a
Kang Choi / 최 강 <109474668+y2hscmtk@users.noreply.github.com>
2024-07-20 00:15:14 +0900

    ✅ OAuth2-JWT-API : JWT 발급 및 검증 클래스 작성

---
+++ oauth2-jwt-api/src/main/java/com/example/oauth2_jwt_api/jwt/JWTUtil.java
@@ -0,0 +1,57 @@
+package com.example.oauth2_jwt_api.jwt;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+
+@Component
+public class JWTUtil {
+
+    private final SecretKey secretKey;
+
+    public JWTUtil(@Value("${spring.jwt.secret}") String secret) {
+        secretKey = new SecretKeySpec(secret.getBytes(StandardCharsets.UTF_8), Jwts.SIG.HS256.key().build().getAlgorithm());
+    }
+
+    public Claims getJwtPayloads(String token) {
+        return Jwts.parser()
+                .verifyWith(secretKey)
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
+
+    // 사용자 이름 반환
+    public String getUsername(String token) {
+        return getJwtPayloads(token)
+                .get("username", String.class);
+    }
+
+    public String getRole(String token) {
+        return getJwtPayloads(token)
+                .get("role", String.class);
+    }
+
+    public Boolean isExpired(String token) {
+        return getJwtPayloads(token)
+                .getExpiration().before(new Date());
+    }
+
+    // JWT 발급
+    public String createJwt(String username, String role, Long expiredMs) {
+
+        return Jwts.builder()
+                .claim("username", username)
+                .claim("role", role)
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + expiredMs))
+                .signWith(secretKey)
+                .compact();
+    }
+}

1b485d5470941e1470937b2a351a0c8071852fff
Kang Choi / 최 강 <109474668+y2hscmtk@users.noreply.github.com>
2024-10-29 21:19:07 +0900

    🎉 소켓 통신(채팅 구현) 프로젝트 시작

---
+++ socket-chat/src/test/java/choi76/socket_chat/SocketChatApplicationTests.java
@@ -0,0 +1,13 @@
+package choi76.socket_chat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocketChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

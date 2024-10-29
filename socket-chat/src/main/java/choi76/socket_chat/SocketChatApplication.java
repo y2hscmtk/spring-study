74ceb9833c335f1d7033a8ca9471501e9ad7d823
Kang Choi / 최 강 <109474668+y2hscmtk@users.noreply.github.com>
2024-10-29 21:21:45 +0900

    ➕ [chore] 웹소켓 의존성 추가

--- socket-chat/src/main/java/choi76/socket_chat/SocketChatApplication.java
+++ socket-chat/src/main/java/choi76/socket_chat/SocketChatApplication.java
@@ -2,8 +2,14 @@ package choi76.socket_chat;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+/**
+ * reference : https://velog.io/@sunkyuj/Spring-%EC%9B%B9%EC%86%8C%EC%BC%93%EC%9C%BC%EB%A1%9C-%EC%8B%A4%EC%8B%9C%EA%B0%84-%EC%B1%84%ED%8C%85-%EA%B5%AC%ED%98%84
+ */
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class SocketChatApplication {
 
 	public static void main(String[] args) {

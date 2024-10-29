2179723b492ec34419e27577fe778ccb537c4e88
Kang Choi / 최 강 <109474668+y2hscmtk@users.noreply.github.com>
2024-10-30 02:56:17 +0900

    ✨ [feat] 새로운 채팅방 생성 기능 작성

--- socket-chat/src/main/java/choi76/socket_chat/domain/chat/controller/ChatController.java
+++ socket-chat/src/main/java/choi76/socket_chat/domain/chat/controller/ChatController.java
@@ -3,7 +3,11 @@ package choi76.socket_chat.domain.chat.controller;
 import choi76.socket_chat.domain.chat.service.ChatService;
 import choi76.socket_chat.global.socket.handler.WebSocketChatHandler;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,4 +16,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class ChatController {
     private final ChatService chatService;
     private final WebSocketChatHandler webSocketChatHandler;
+
+    /**
+     * 새로운 채팅방 생성
+     */
+    @PostMapping("/create")
+    public ResponseEntity<?> createChatRoom(@RequestParam("ownerId") Long ownerId) {
+        return chatService.createChatRoom(ownerId);
+    }
 }
--- socket-chat/src/main/java/choi76/socket_chat/domain/chat/service/ChatService.java
+++ socket-chat/src/main/java/choi76/socket_chat/domain/chat/service/ChatService.java
@@ -1,13 +1,32 @@
 package choi76.socket_chat.domain.chat.service;
 
+import choi76.socket_chat.domain.chat.entity.Chatroom;
 import choi76.socket_chat.domain.chat.repository.ChatRepository;
 import choi76.socket_chat.domain.chat.repository.ChatroomRepository;
+import choi76.socket_chat.domain.member.entity.Member;
+import choi76.socket_chat.domain.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Transactional
 @RequiredArgsConstructor
 public class ChatService {
-    private ChatroomRepository chatroomRepository;
-    private ChatRepository chatRepository;
+    private final ChatroomRepository chatroomRepository;
+    private final ChatRepository chatRepository;
+    private final MemberRepository memberRepository;
+
+    public ResponseEntity<?> createChatRoom(Long ownerId) {
+        Member owner = memberRepository.findById(ownerId).get();
+
+        // 편의상 검증 로직 생략
+        Chatroom newChatRoom = Chatroom.builder()
+                .owner(owner)
+                .title("테스트")
+                .build();
+        chatroomRepository.save(newChatRoom);
+        return ResponseEntity.ok("채팅방이 생성되었습니다 아이디 : " + newChatRoom.getId());
+    }
 }
